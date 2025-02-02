5761d272d9d26b0a55d7a3a8b763456de2d9817d
GadeKuldeep <gadekuldeep25@gmail.com>
2025-02-02 23:57:31 +0530

    shift mongo db to online server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -18,8 +20,8 @@ app.use('/person',personRoutes);
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/menu', menuRoutes);
 
+const PORT=process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose =require('mongoose');
-
+require('dotenv').config();
 // Define the MongoDB connection URL
-const mongoURL='mongodb://localhost:27017/hotels' //replace  hotels with your database name
-
+// const mongoURL=process.env.MONGODB_URL_LOCAL //replace  hotels with your database name
+const mongoURL=process.env.MONGODB_URL;
 // setup mongodb connection
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
